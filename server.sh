9160df85cd9b6c90389257bb095e11afdcca0779
MaxiD1 <maximiliano.duarte@usm.cl>
2026-06-10 22:22:08 -0300

    Script para correr un server local
    Script para crear un servidor local para poder usar la pagina.
    
    Se puede acceder a la pagina desde http://localhost:8000/

---
+++ server.sh
@@ -0,0 +1,9 @@
+# http://localhost:8000/
+# Si tienes Python 3 instalado:
+python -m http.server 8000
+
+# O con Python 2:
+python -m SimpleHTTPServer 8000
+
+# O con Node.js (si tienes instalado):
+npx http-server
